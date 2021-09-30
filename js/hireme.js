17062ff13e075af8ffe29581e1d928d2d9d3ace6
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2021-09-30 23:49:12 +0530

    responsive page 2

--- js/hireme.js
+++ js/hireme.js
@@ -7,6 +7,8 @@ const menuIcon = document.getElementById("menu-icon");
 const closeIcon = document.getElementById("close-icon");
 const menu = document.getElementById("menu");
 
+const links = document.querySelectorAll(".mobile-link");
+
 menuIcon.addEventListener("click", () =>
 {
     menu.classList.toggle("open");
@@ -15,4 +17,10 @@ menuIcon.addEventListener("click", () =>
 closeIcon.addEventListener("click", () =>
 {
     menu.classList.remove("open");
+})
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        menu.classList.toggle("open");
+    });
 })
